da837be9fe9649786bdf721aad3fe06b39bc7555
LvPin <lp871681354@126.com>
2021-07-07 14:29:28 -0400

    Update get_LRL.R

--- R/get_LRL.R
+++ R/get_LRL.R
@@ -10,8 +10,10 @@
 #'  myLRL: A list of two elements:
 #'  myLRL$'L1->L2_L2->L1': LRloop network with L1->L2 and L2->L1 (L2 is a target of L1 and L1 is a target of L2)
 #'  myLRL$'R1->L2_R2->L1': LRloop network with R1->L2 and R2->L1 (L2 is a target of R1 and L1 is a target of R2)
+#'  myLRL$'eachcondition': LRLoop networks in each condition
 #' @import nichenetr tidyverse Seurat
 #' @export
+
 
 
 
@@ -24,6 +26,8 @@ get_LRL <- function(lr_expr_ct2_to_ct1, lr_expr_ct1_to_ct2,
     LRL_list[[i]] = make_LRL_list(lr_expr_ct2_to_ct1$eachcondition[[i]], lr_expr_ct1_to_ct2$eachcondition[[i]],
                                       ligand_target_matrix_binary_ct2_to_ct1, ligand_target_matrix_binary_ct1_to_ct2,
                                       receptor_target_matrix_binary_ct2_to_ct1, receptor_target_matrix_binary_ct1_to_ct2)
+    LRL_list[[i]] = list(LRL_list[[i]]$`L1->L2 & L2->L1`, LRL_list[[i]]$`R1->L2 & R2->L1`)
+    names(LRL_list[[i]]) = c('L1->L2 & L2->L1', 'R1->L2 & R2->L1')
   }
   names(LRL_list) = names(lr_expr_ct1_to_ct2$eachcondition)
   
@@ -38,8 +42,8 @@ get_LRL <- function(lr_expr_ct2_to_ct1, lr_expr_ct1_to_ct2,
   colnames(LRL_list_bind_L1L2L2L1) = c('L2','R2', 'L1', 'R1')
   colnames(LRL_list_bind_R1L2R2L1) = c('L2','R2', 'L1', 'R1')
   
-  myLRL = list(LRL_list_bind_L1L2L2L1, LRL_list_bind_R1L2R2L1)
-  names(myLRL) = c('L1->L2_L2->L1', 'R1->L2_R2->L1')
+  myLRL = list(LRL_list_bind_L1L2L2L1, LRL_list_bind_R1L2R2L1, LRL_list)
+  names(myLRL) = c('L1->L2_L2->L1', 'R1->L2_R2->L1', 'eachcondition')
   
   return(myLRL)
 }
