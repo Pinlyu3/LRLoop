6e73cc6e79d847fffe54995cf5e1fb8e4962d59a
LvPin <lp871681354@126.com>
2021-07-07 14:32:50 -0400

    Update get_LRscores.R

--- R/get_LRscores.R
+++ R/get_LRscores.R
@@ -1,4 +1,4 @@
-#' Calculate the LRscore of each "expressed" ligand-receptor pair in each condition
+#' Calculate the LRscore of each "expressed" ligand-receptor pair in each condition.  Only run after running "PrepareBasics".
 #' @name get_LRscores 
 #' @param lr_expr A ligand-receptor network matrix with two columns "from" and "to".
 #' @param conditions A vector of conditions of interest
@@ -6,6 +6,8 @@
 #' @param value.use_to A numeric matrix with genes in rows and conditions in columns, selected as the receptor values to be used in the calculation of LRscores
 #' @param scalar A number, plays the role of a scalar in the calculation of LRscores when the LRscore_method set to 'scsigr' or individual_scale'
 #' @param LRscore_method The method of calculating the LRscores, available options are 'mean', 'individual_scale', 'individual_scale_exp', 'product', 'bias_receptor' and 'scsigr' 
+#' @param LRL_eachcondition List of LRloop networks in each condition.
+#' @param LRL_filter "L1R1", "L2R2" or "none".  "L1R1" ("L2R2"): The LR pairs are from ct1 (ct2) to ct2 (ct1) and apply the LRloop filter in each condition, that is, in each condition, an LRscore will be set to 0 if the LR pair does not form any LRloop in that conditon. "none": No LRloop filter applied. 
 #' @return
 #'  A matrix with columns "ligand", "receptor" and the condition names that keeps record of the LRscore of each ligand-receptor pair in each condition 
 #' @import nichenetr tidyverse Seurat
@@ -13,7 +15,7 @@
 
 
 
-get_LRscores <- function(lr_expr, conditions, value.use_from, value.use_to, scalar, LRscore_method) {
+get_LRscores <- function(lr_expr, conditions, value.use_from, value.use_to, scalar, LRscore_method, LRL_eachcondition, LRL_filter = 'none') {
   
   LRscorematrix = matrix(0, nrow = nrow(lr_expr), ncol = (2+length(conditions)))
   colnames(LRscorematrix) = c('from', 'to', conditions)
@@ -26,6 +28,28 @@ get_LRscores <- function(lr_expr, conditions, value.use_from, value.use_to, scal
     }
     LRscorematrix[i,] = c(ligand, receptor, lrscores)
   }
+  
+  if (LRL_filter == "L1R1") {
+    for (j in 1:length(conditions)) {
+      cond = conditions[j]
+      LRset = unique(LRL_eachcondition[[cond]][['R1->L2 & R2->L1']][,c("L1", "R1")])
+      for (i in 1:nrow(LRscorematrix)) {
+        idx = which(LRset[,'L1']==LRscorematrix[i,'from'] & LRset[,'R1']==LRscorematrix[i,'to'])
+        LRscorematrix[i,cond] = as.numeric(LRscorematrix[i,cond])*(length(idx)>0)
+      }
+    }
+  } else if (LRL_filter == "L2R2") {
+    for (j in 1:length(conditions)) {
+      cond = conditions[j]
+      LRset = unique(LRL_eachcondition[[cond]][['R1->L2 & R2->L1']][,c("L2", "R2")])
+      for (i in 1:nrow(LRscorematrix)) {
+        idx = which(LRset[,'L2']==LRscorematrix[i,'from'] & LRset[,'R2']==LRscorematrix[i,'to'])
+        LRscorematrix[i,cond] = as.numeric(LRscorematrix[i,cond])*(length(idx)>0)
+      }
+    }
+  }
+  
+  
   
   return(LRscorematrix)
 }
