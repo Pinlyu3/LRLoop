e1daee828c29755a2f9b78940a07170f0d0a383b
LvPin <lp871681354@126.com>
2022-01-04 14:53:12 -0500

    Update get_pct_expr.R

--- R/get_pct_expr.R
+++ R/get_pct_expr.R
@@ -4,22 +4,16 @@
 #' @param conditions A vector of conditions of interest
 #' @return
 #'  A matrix of the detection rate of each gene in each condition with genes in rows and conditions in columns
-#' @import nichenetr tidyverse Seurat
+#' @import tidyverse Seurat
 #' @export
 
 
-
-
-
 get_pct_expr <- function(seuratobj, conditions) {
-  library(Seurat)
-  library(Matrix)
   data = seuratobj@assays$RNA@data
   pct_expr = matrix(0, nrow = nrow(data), ncol = length(conditions))
   Idents(object = seuratobj) = 'Condition'
   for (i in 1:length(conditions)) {
     subdata = data[,WhichCells(seuratobj, idents = conditions[i])]
-    print(dim(subdata))
     pct_expr[,i] = rowSums(subdata >0)/ncol(subdata)
   }
   rownames(pct_expr) = rownames(data)
